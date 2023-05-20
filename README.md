274f185c735ca64f8e1fb1bde881b2a62196a887
Sar1n0196 <sushrutkulkarni0196@gmail.com>
2023-05-20 10:58:06 +0530

    switched to node

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-## To download requirements:
-pip install -r requirements.txt
+## Requirements:
+* NodeJS
+* Xano SDK: npm install @xano/js-sdk
+
+## How to run:
+* Clone this repo
+* Run `npm install`
+* Enter Xano API Key in `index.js`
+* Run `node index.js`
+* Open your browser and go to `localhost:3000`
+
+
---
+++ index.js
@@ -0,0 +1,40 @@
+const xano = require("@xano/js-sdk");
+
+const client = new xano.Client("Enter_Xano_API_Key_Here");
+
+// POST /generateClaim
+exports.generateClaim = async (req, res) => {
+  const { walletAddress } = req.body;
+
+  // Generate claim from Reclaim SDK
+  const claim = await Reclaim.generateClaim(walletAddress);
+
+  // Send back the response template to the TPH website
+  res.json({
+    claim,
+  });
+};
+
+// GET /claimStatus
+exports.getClaimStatus = async (req, res) => {
+  const { claimId } = req.query;
+
+  // Fetch the claim status
+  const claimStatus = await client.query("SELECT * FROM claims WHERE id = ?", claimId);
+
+  // Loading UI on webflow
+  res.json({
+    claimStatus,
+  });
+};
+
+// UPDATE /claimStatus
+exports.updateClaimStatus = async (req, res) => {
+  const { claimId, status } = req.body;
+
+  // Update the claim after usrr has completed the claim in Reclaim
+  await client.query("UPDATE claims SET status = ? WHERE id = ?", status, claimId);
+
+  // Redirect to the success or failure page
+  res.redirect(status === "success" ? "/success" : "/failure");
+};
